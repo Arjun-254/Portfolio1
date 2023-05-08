e40fd80b3f3dd84cb37c1df6052d0efa0717528f
Arjun-254 <arjun.shah254@gmail.com>
2023-05-08 18:28:25 +0530

    first

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,21 +40,6 @@ export default function Navbar() {
         <div className="lg:hidden" role="dialog" aria-modal="true">
         
         <div className="fixed inset-0 z-50"></div>
-        <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
-                <img className="h-8 w-auto" src={image} alt="" />
-            </a>
-            <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700">
-                <span className="sr-only">Close menu</span>
-                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-            </button>
-            </div>
-            
-        </div>
         </div>
     </header>
 
