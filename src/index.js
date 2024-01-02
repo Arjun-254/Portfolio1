4e35245d484b32eb2bb55c7afeb1a499729426a9
Arjun-254 <arjun.shah254@gmail.com>
2024-01-03 03:08:57 +0530

    speed insights vercel

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
     <App />
     <Analytics />
+    <SpeedInsights />
   </div>
 );
