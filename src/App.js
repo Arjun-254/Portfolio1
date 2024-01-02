5005de85c2cf93e6e605ce1cd16b9cbb6772c3f2
Arjun-254 <arjun.shah254@gmail.com>
2024-01-03 03:02:05 +0530

    vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { BrowserRouter as Router ,Routes,Route} from 'react-router-dom'
-import About from './components/About'
-import Hero from './components/Hero'
-import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <Router>
-      <div className='relative z-0 bg-gradient-to-r from-black via-gray-900 to-black'>
-        <div className='bg-cover bg-no-repeat bg-center' > 
-          <Navbar/>
-          <Hero/>
+      <div className="relative z-0 bg-gradient-to-r from-black via-gray-900 to-black">
+        <div className="bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
         </div>
-        <About/> 
+        <About />
       </div>
     </Router>
-  )
+  );
 }
