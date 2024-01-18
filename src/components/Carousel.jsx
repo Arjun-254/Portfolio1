11210efece08e1286277a0238c179f1fc2500cdf
Arjun-254 <arjun.shah254@gmail.com>
2024-01-19 01:13:47 +0530

    resized photos on web

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -28,20 +28,20 @@ export const Carousel = () => {
   };
 
   return (
-    <div className="w-5/6 sm:w-5/6 d:w-4/6 mx-auto mt-10">
+    <div className="w-5/6 sm:w-5/6 md:w-4/6 mx-auto mt-10 rounded-md">
       <Slider {...settings}>
         {images.map((image, index) => (
           <div
             key={index}
             className="relative h-[40vh] lg:h-[60vh] rounded-md overflow-hidden"
           >
-            <div className="group relative flex items-center justify-center h-full">
+            <div className="group relative flex items-center justify-center h-full ">
               <img
                 src={image}
                 alt={`Image ${index + 1}`}
-                className="w-full h-full -z-0 absolute rounded-md transition-all transform group-hover:scale-105"
+                className="w-full h-full -z-0 absolute rounded-md transition-all transform group-hover:scale-105 "
               />
-              <div className="absolute inset-0 flex items-center justify-center text-white text-lg opacity-80 sm:opacity-0 group-hover:opacity-100 transition-all bg-black bg-opacity-90 rounded-md">
+              <div className="absolute inset-0 flex items-center justify-center text-white text-lg opacity-80 sm:opacity-0 group-hover:opacity-100 transition-all bg-black bg-opacity-90 rounded-md ">
                 <div className="text-center ">
                   <p className="text-3xl sm:text-4xl md:text-5xl font-extrabold font-sans">
                     {data[index].name}
