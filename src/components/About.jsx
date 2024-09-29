edcfa4bd2d0c658dc99ac0db7475e8085713869f
Arjun-254 <arjun.shah254@gmail.com>
2024-09-29 11:40:06 +0530

    bolded text in about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -128,7 +128,7 @@ const About = () => {
       >
        I am a <strong className="text-white">full-stack and machine learning developer</strong> currently pursuing a major in Computer Science. 
        I am excited to begin my role as a <strong className="text-white">UIUC+ Summer Research Intern.</strong> 
-       {' '}Previously, I gained valuable experience as a Software Development Engineer (SDE) intern at HDFC Securities. 
+       {' '}Previously, I gained valuable experience as a <strong className="text-white">Software Development Engineer (SDE) intern at HDFC Securities.</strong>
        My diverse background includes both academic and research experiences, complementing my skills in software development and machine learning.
       </p>
 
